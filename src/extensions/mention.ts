d42502b7e3630432ca880106d347fb7a629d9ea0
Joel <hejo03@gmx.de>
2025-02-19 19:17:23 +0100

    feat(mention): lint
    Signed-off-by: Joel <hejo03@gmx.de>

--- src/extensions/mention.ts
+++ src/extensions/mention.ts
@@ -1,13 +1,9 @@
 import type { GeneralOptions } from '@/type'
 import type { MentionOptions as TiptapMentionOptions } from '@tiptap/extension-mention'
-// import type { EditorView } from '@tiptap/pm/view'
-// import { getMarkRange } from '@tiptap/core'
 import { Mention as TiptapMention } from '@tiptap/extension-mention'
 
-// import { Plugin, TextSelection } from '@tiptap/pm/state'
-
 /**
- * Represents the interface for link options, extending TiptapLinkOptions and GeneralOptions.
+ * Represents the interface for mention options, extending TiptapMentionOptions and GeneralOptions.
  */
 export interface MentionOptions extends TiptapMentionOptions, GeneralOptions<MentionOptions> { }
 
